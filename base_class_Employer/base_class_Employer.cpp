033ad08cbb4bb907ac4c9ddd8ef5e91f16b0a56c
Макс E <max0681832239@hotmail.com>
2021-10-03 12:04:58 +0300

    Добавьте файлы проекта.

---
+++ base_class_Employer/base_class_Employer.cpp
@@ -0,0 +1,84 @@
+﻿// base_class_Employer.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+class Employer
+{
+protected:
+	string name;
+	int age;
+	string posada;
+public:
+	Employer(string name,int age,string posada) {
+		this->name = name;
+		this->age = age;
+		this->posada = posada;
+	}
+	~Employer() = default;
+
+	virtual void Print() = 0;
+};
+
+class President:public Employer
+{
+public:
+	President(string name, int age, string posada) :Employer(name,age,posada)
+	{
+		this->name = name;
+		this->age = age;
+		this->posada = posada;
+	}
+	void Print()
+	{
+		cout << name << " " << age << " " << posada << endl;
+	}
+
+};
+
+class Manager :public Employer
+{
+public:
+	Manager(string name, int age, string posada) :Employer(name, age, posada)
+	{
+		this->name = name;
+		this->age = age;
+		this->posada = posada;
+	}
+	void Print()
+	{
+		cout << name << " " << age << " " << posada << endl;
+	}
+};
+
+class Worker :public Employer
+{
+public:
+	Worker(string name, int age, string posada) :Employer(name, age, posada)
+	{
+		this->name = name;
+		this->age = age;
+		this->posada = posada;
+	}
+	void Print()
+	{
+		cout << name << " " << age << " " << posada << endl;
+	}
+};
+
+int main()
+{
+	President* president = new President("Vasya Pupkin",33," president");
+	president->Print();
+
+	Manager* manager = new Manager("Ivan Ivanov", 30, "manager");
+	manager->Print();
+
+	Worker* worker = new Worker("Petr Petrov", 24, "worker");
+	worker->Print();
+
+}
+
